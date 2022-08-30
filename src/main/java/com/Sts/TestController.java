a25b47c34b7b87da14e224400103a857b7c41c52
dhdharshini <dhivya.dharshini@soprasteria.com>
2022-08-30 16:23:46 +0530

    commit abc

---
+++ src/main/java/com/Sts/TestController.java
@@ -0,0 +1,22 @@
+package com.Sts;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+	@RequestMapping("/test")
+	public String home()
+	{
+		return "home";
+		
+	}
+
+	@RequestMapping("/contact")
+	public String contact()
+	{
+		return "contact";
+	}
+
+}
